8af1e0c6ef265c0235806979d896abe66282c8ec
Ankita Mohapatra <anksstudyuse@gmail.com>
2025-12-21 20:06:39 +0530

    Add files via upload

---
+++ DAY5_move_zeros.java
@@ -0,0 +1,17 @@
+
+public class DAY5_move_zeros {
+	public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+
+    }
+
+}
---
+++ DAY5_remove_duplicates.java
@@ -0,0 +1,18 @@
+
+public class DAY5_remove_duplicates {
+
+	public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int insertPos = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+        return insertPos;
+
+	}
+
+}
